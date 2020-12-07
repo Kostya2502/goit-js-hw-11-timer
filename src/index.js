6dcb9f3c4639599c4cff9fb5633341f1bd0da765
Kostya Panasiuk <kos2502@ukr.net>
2020-12-07 18:06:10 +0200

    fin

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import './styles.css';
+
+const refs = {
+    timer: document.querySelector('#timer-1'),
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[data-value="hours"]'),
+    mins: document.querySelector('[data-value="mins"]'),
+    secs: document.querySelector('[data-value="secs"]')
+}
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate
+    }
+
+    interval = setInterval(() => {
+        const dayNow = Date.now()
+        const time = this.targetDate - dayNow
+        this.toCount(time)
+        this.closeCount(time)
+    }, 1000)
+
+    toCount(time) {
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+        const secs = Math.floor((time % (1000 * 60)) / 1000);
+        refs.days.textContent = `${days}`
+        refs.hours.textContent = `${hours}`
+        refs.mins.textContent = `${mins}`
+        refs.secs.textContent = `${secs}`
+    }
+
+    pad(value) { return String(value).padStart(2, "0"); }
+
+    closeCount(time) { if (time < 0) { clearInterval(this.interval) } }
+}
+
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Dec 31, 2020'),
+});
+
+
+
+
+
+
+
+
+
+
+
+
