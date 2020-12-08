be9cee260d07b63f24bfbc06646612d3ba6e29b5
Kostya Panasiuk <kos2502@ukr.net>
2020-12-08 19:13:53 +0200

    not problems

--- src/index.js
+++ src/index.js
@@ -12,13 +12,6 @@ class CountdownTimer {
     constructor({ selector, targetDate }) {
         this.selector = selector;
         this.targetDate = targetDate;
-        this.refs = {
-            timer: document.querySelector('#timer-1'),
-            days: document.querySelector('[data-value="days"]'),
-            hours: document.querySelector('[data-value="hours"]'),
-            mins: document.querySelector('[data-value="mins"]'),
-            secs: document.querySelector('[data-value="secs"]')
-        }
     }
 
     interval = setInterval(() => {
@@ -33,10 +26,16 @@ class CountdownTimer {
         const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
         const secs = Math.floor((time % (1000 * 60)) / 1000);
-        this.refs.days.textContent = `${days}`
-        this.refs.hours.textContent = `${hours}`
-        this.refs.mins.textContent = `${mins}`
-        this.refs.secs.textContent = `${secs}`
+
+        const timer1 = document.querySelector(`${this.selector}`)
+        const daysR = timer1.querySelector('[data-value="days"]')
+        const hoursR = timer1.querySelector('[data-value="hours"]')
+        const minutesR = timer1.querySelector('[data-value="mins"]')
+        const secondsR = timer1.querySelector('[data-value="secs"]')
+        daysR.textContent = `${days}`
+        hoursR.textContent = `${hours}`
+        minutesR.textContent = `${mins}`
+        secondsR.textContent = `${secs}`
     }
 
     pad(value) { return String(value).padStart(2, "0"); }
@@ -48,15 +47,3 @@ new CountdownTimer({
     selector: '#timer-1',
     targetDate: new Date('Dec 31, 2020'),
 });
-
-
-
-
-
-
-
-
-
-
-
-
